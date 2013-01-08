7bb2c3ae7c0d57f26a5d466fef2183dada84342d
Sanpi <sanpi@homecomputing.fr>
2013-01-08 17:32:45 +0100

    Add pomm service

---
+++ src/Sanpi/Cist/Resources/config/development.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'pomm.dsn' => array(
+        'cist' => array(
+            'dsn' => 'pgsql://sanpi@172.17.0.1:5432/cist',
+            'name' => 'cist'
+        ),
+    ),
+);
--- src/app.php
+++ src/app.php
@@ -1,14 +1,19 @@
 <?php
 
-use Silex\Application;
-use Sanpi\Coding\Rule;
+use Pomm\Silex\PommServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
-$app = new Application();
+$app = new Silex\Application();
+
+$app['config'] = require __DIR__ . '/Sanpi/Cist/Resources/config/current.php';
 
 $app->register(new TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/Sanpi/Cist/Resources/views',
+));
+
+$app->register(new PommServiceProvider(), array(
+    'pomm.databases' => $app['config']['pomm.dsn'],
 ));
 
 $app->get('/', function(Request $request) use($app) {
