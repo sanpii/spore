d684fa8a6eb76d811c133560861f5d86657a4db3
Sanpi <sanpi@homecomputing.fr>
2013-01-09 11:51:50 +0100

    Remove pomm service provider

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -13,8 +13,12 @@ $app->register(new TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/views',
 ));
 
-$app->register(new PommServiceProvider(), array(
-    'pomm.databases' => $app['config']['pomm'],
-));
+$app['pomm.service'] = $app->share(function() use ($app) {
+    return new Pomm\Service($app['config']['pomm']);
+});
+
+$app['pomm'] = function() use ($app) {
+    return $app['pomm.service']->createConnection();
+};
 
 return $app;
