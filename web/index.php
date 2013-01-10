9bd61c8a0f2a178a67641331f310d3dc336fa749
Sanpi <sanpi@homecomputing.fr>
2013-01-10 10:14:01 +0100

    src/app.php -> src/application.php

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
 }
 
-$app = require __DIR__ . '/../src/app.php';
+$app = require __DIR__ . '/../src/application.php';
 if (php_sapi_name() === 'cli-server') {
     $app['debug'] = true;
 }
