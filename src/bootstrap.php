4bd3df2b581a3d47803c8c5ad9b4033f187fd15b
Sanpi <sanpi@homecomputing.fr>
2014-12-10 10:57:13 +0100

    Use composer parameter handler

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,15 +5,31 @@ use \Pomm\Silex\PommServiceProvider;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (!is_file(__DIR__ . '/config/current.yml')) {
-    throw new \RunTimeException('No current configuration file found in config.');
-}
-
 $app = new Silex\Application();
 
-$app['config'] = function () {
-    return Yaml::parse(__DIR__ . '/config/current.yml');
-};
+$app['config'] = $app->share(function () {
+    if (!is_file(__DIR__ . '/config/parameters.yml')) {
+        throw new \RunTimeException('No current configuration file found in config.');
+    }
+
+    $config = Yaml::parse(__DIR__ . '/config/parameters.yml');
+    $parameters = $config['parameters'];
+
+    $parameters['pomm'] = [
+        $parameters['project_name'] => [
+            'dsn' => sprintf(
+                "pgsql://%s:%s@%s:%s/%s",
+                $parameters['database_user'],
+                $parameters['database_password'],
+                $parameters['database_host'],
+                $parameters['database_port'],
+                $parameters['database_name']
+            ),
+        ],
+    ];
+
+    return $parameters;
+});
 
 $app['debug'] = $app['config']['debug'];
 
