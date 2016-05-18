db3769e874dd7d61c8f1c86a484633ee0f3c8cd3
Sanpi <sanpi@homecomputing.fr>
2016-05-18 10:45:19 +0200

    Use php 7

--- src/application.php
+++ src/application.php
@@ -1,11 +1,12 @@
 <?php
+declare(strict_types = 1);
 
 use \Silex\Application;
 use \Symfony\Component\HttpFoundation\Request;
 
 $app = require __DIR__ . '/bootstrap.php';
 
-$app->get('/', function(Application $app, Request $request) {
+$app->get('/', function(Application $app, Request $request): string {
     return $app['twig']->render('index.html.twig');
 });
 
